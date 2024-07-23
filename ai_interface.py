8628375b142ac2f164e6eeb00b5171187bd28222
Kyler Li <loukylor@gmail.com>
2024-07-23 00:08:47 -0700

    finalize ai interface

--- ai_interface.py
+++ ai_interface.py
@@ -2,6 +2,9 @@ from logging import Logger
 import logging
 from multiprocessing.connection import Client, Connection
 from threading import Thread
+import time
+import traceback
+from typing import Any, Callable
 from uuid import UUID
 
 import orjson
@@ -10,19 +13,37 @@ from db import ModelPreset
 
 
 class AIInterface:
-    def __init__(self, host: str, port: int, authkey: bytes, logger: Logger = None) -> None:
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        authkey: bytes,
+        callback: Callable[[bytes], Any],
+        logger: Logger = None
+    ) -> None:
         self.host = host
         self.port = port
         self.authkey = authkey
+        self.callback = callback
         self.logger = logger or logging.getLogger("AI Interface")
 
-        self._client: Connection
+        self._client: Connection = None
         self._thread = Thread(target=self._thread_loop)
         self._run_loop = True
 
     def start(self) -> None:
-        self._client = Client((self.host, self.port), authkey=self.authkey)
+        self._try_create_client()
         self._thread.start()
+
+    def _try_create_client(self) -> None:
+        try:
+            self._client = Client((self.host, self.port), authkey=self.authkey)
+            self.logger.info("Connected to target address")
+            from constants import DB
+            self.set_preset(UUID("10a8827c-9187-4dc6-9c48-c883af32ebc4"), DB.get_user_model_presets("10a8827c-9187-4dc6-9c48-c883af32ebc4")[0])
+            self.send_text_data(UUID("10a8827c-9187-4dc6-9c48-c883af32ebc4"), "hi")
+        except ConnectionRefusedError:
+            self.logger.warning("Interface could not connect to target address")
 
     def __enter__(self) -> "AIInterface":
         self.start()
@@ -31,12 +52,16 @@ class AIInterface:
     def close(self) -> None:
         self._thread.join()
         self._run_loop = False
-        self._client.close()
+        if self._client is not None:
+            self._client.close()
 
     def __exit__(self, *args) -> None:
         self.close()
 
     def set_preset(self, user_id: UUID, preset: ModelPreset):
+        if self._check_client_is_none():
+            return
+
         self._client.send_bytes(orjson.dumps({
             "op": 1,
             "id": user_id,
@@ -44,30 +69,69 @@ class AIInterface:
         }))
 
     def remove_preset(self, user_id: UUID):
+        if self._check_client_is_none():
+            return
+
         self._client.send_bytes(orjson.dumps({
             "op": 2,
             "id": user_id
         }))
 
     def send_voice_data(self, user_id: UUID, data: bytes):
-        self._client.send_bytes(bytes(3) + user_id.bytes + data)
+        if self._check_client_is_none():
+            return
+
+        self._client.send_bytes((3).to_bytes() + user_id.bytes + data)
 
     def send_text_data(self, user_id: UUID, data: str):
+        if self._check_client_is_none():
+            return
         self._client.send_bytes(orjson.dumps({
             "op": 4,
             "id": user_id,
             "data": data
         }))
 
+    def _check_client_is_none(self):
+        if self._client is None:
+            self.logger.warning("Attempted to use client while it is none")
+            return True
+        return False
+
     def _thread_loop(self):
         while True:
-            if not self._run_loop:
-                break
+            try:
+                if not self._run_loop:
+                    return
 
-            if self._client.poll(1):
-                print(self._client.recv_bytes())
+                if self._client is None or self._client.closed:
+                    self._try_create_client()
+                    time.sleep(1)
+                    continue
+
+                if not self._client.poll(1):
+                    continue
+
+                try:
+                    payload = self._client.recv_bytes()
+                except (EOFError, OSError):
+                    self._client.close()
+                    self._client = None
+
+                try:
+                    callback = self.callback
+                    callback(payload)
+                except Exception: # pylint: disable=broad-exception-caught
+                    self.logger.error("Error during callback:\n%s", traceback.format_exc())
+
+            except Exception: # pylint: disable=broad-exception-caught
+                self.logger.error("Exception in interface loop:\n%s",traceback.format_exc())
+
 
 if __name__ == "__main__":
     from constants import AIINTERFACE, DB
-    AIINTERFACE.set_preset(UUID("10a8827c-9187-4dc6-9c48-c883af32ebc4"), DB.get_user_model_presets("10a8827c-9187-4dc6-9c48-c883af32ebc4")[0])
-    AIINTERFACE._thread.join()
+    try:
+        AIINTERFACE._thread.join()
+    except KeyboardInterrupt:
+        AIINTERFACE.close()
+        DB.close()
